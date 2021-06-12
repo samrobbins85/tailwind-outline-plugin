bac44cd11887a0572b177cd96150f38eca5b7fd5
samrobbins85 <samrobbinsgb@gmail.com>
2021-06-12 18:39:10 +0100

    🎉 Start the project

---
+++ index.js
@@ -0,0 +1,55 @@
+const plugin = require("tailwindcss/plugin");
+
+const flattenColorPalette = (colors) =>
+  Object.assign(
+    {},
+    ...Object.entries(colors).flatMap(([color, values]) =>
+      typeof values == "object"
+        ? Object.entries(flattenColorPalette(values)).map(([number, hex]) => ({
+            [color + (number === "DEFAULT" ? "" : `-${number}`)]: hex,
+          }))
+        : [{ [`${color}`]: values }]
+    )
+  );
+
+const outlinePlugin = plugin(
+  function ({ addUtilities, e, theme, variants }) {
+    const width = Object.entries(theme("spacing")).map(([key, value]) => {
+      return { [`.${e(`outline-${key}`)}`]: { outlineWidth: `${value}` } };
+    });
+    const style = Object.entries(theme("outlineStyle")).map(([key, value]) => {
+      return {
+        [`.${e(`outline-${key}`)}`]: { outlineStyle: `${value}` },
+      };
+    });
+    const palette = flattenColorPalette(theme("colors"));
+    const color = Object.entries(palette).map(([key, value]) => {
+      return {
+        [`.${e(`outline-${key}`)}`]: { outlineColor: `${value}` },
+      };
+    });
+    const offset = Object.entries(theme("spacing")).map(([key, value]) => {
+      return {
+        [`.${e(`outline-offset-${key}`)}`]: { outlineOffset: `${value}` },
+      };
+    });
+    const utilities = [...width, ...style, ...color, ...offset];
+    addUtilities(utilities, variants("outline"));
+  },
+  {
+    theme: {
+      outlineStyle: {
+        solid: "solid",
+        dotted: "dotted",
+        groove: "groove",
+        none: "none",
+        inset: "inset",
+      },
+    },
+    variants: {
+      outline: ["hover", "active", "responsive"],
+    },
+  }
+);
+
+module.exports = outlinePlugin;
